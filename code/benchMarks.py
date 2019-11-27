20f5fb3b128303a2672f2f6ad30420b0628c82bf
robin montferme <robin.montferme@gmail.com>
2019-11-27 19:16:47 +0100

    upload firsst batch of data + v1 of benchmark

--- code/benchMarks.py
+++ code/benchMarks.py
@@ -4,42 +4,40 @@ import myPlayerBook
 import RandomPlayer
 
 players = []
-playerRandom = RandomPlayer.RandomPlayer()
-playerV1 = myPlayer.myPlayer()
-playerV2 = myPlayerBook.myPlayerBook()
-
 data = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
 n = 10
+
 print("V1 vs Random")
-player1 = myPlayer.myPlayer()
-player2 = RandomPlayer.RandomPlayer()
-players.append(player1)
-players.append(player2)
 for i in range(0,n):
-    res  = localGame(players)
+    player1 = myPlayer.myPlayer()
+    player2 = RandomPlayer.RandomPlayer()
+    players.append(player1)
+    players.append(player2)
+    res = localGame(players)
     data[0][res] += 1
     players.clear()
+    print("Game Done:",i)
 print("Random vs V1 - Done")
 
 print("V2 vs Random")
-player1 = myPlayerBook.myPlayerBook()
-player2 = RandomPlayer.RandomPlayer()
-players.append(player1)
-players.append(player2)
 for i in range(0,n):
+    player1 = myPlayerBook.myPlayerBook()
+    player2 = RandomPlayer.RandomPlayer()
+    players.append(player1)
+    players.append(player2)
     res  = localGame(players)
     data[1][res] += 1
     players.clear()
 print("Rand vs V2 - Done")
 
 print("V1 vs V2")
-player1 = myPlayer.myPlayer()
-player2 = myPlayerBook.myPlayerBook()
-players.append(player1)
-players.append(player2)
 for i in range(0,n):
+    player1 = myPlayer.myPlayer()
+    player2 = myPlayerBook.myPlayerBook()
+    players.append(player1)
+    players.append(player2)
     res  = localGame(players)
     data[2][res] += 1
     players.clear()
@@ -48,6 +46,7 @@ print("V1 vs V2 - Done")
 
 print(data)
 for i in range(0,len(data)):
+    print("---------------------")
     blackRate = (data[i][2]/n)*100
     whiteRate = (data[i][1]/n)*100
     drawRate = (data[i][0]/n)*100
